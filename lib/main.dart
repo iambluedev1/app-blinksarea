b89547dc28135e3927197d7fc9052b0d1ca8e7b5
iambluedev <iambluedev@gmx.fr>
2020-08-31 21:23:31 +0200

    dqsd

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'color_helper.dart';
 
@@ -8,7 +9,10 @@ String selectedUrl = 'https://blinksarea.wixsite.com/blinkapp';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(BlinksArea());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) {
+    runApp(new BlinksArea());
+  });
 }
 
 class BlinksArea extends StatelessWidget {
@@ -51,24 +55,17 @@ class _BlinksAreaWebViewState extends State<BlinksAreaWebView> {
     _onStateChanged =
         flutterWebViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
           if (state.type == WebViewState.finishLoad) {
-            flutterWebViewPlugin.evalJavascript(""" 
-              var styleNode = document.createElement('style');
-              styleNode.type = "text/css";
-              var styleText = document.createTextNode('#c1dmpinlineContent-gridContainer{margin-top: 0px!important} #comp-kef0558m{left: 0!important;justify-self: normal!important;align-self: normal!important;} #SITE_ROOT{ top: 0 !important} #SITE_HEADER{ margin-top:0!important } body{ background-color:#e8e6e7!important } #TPASection_keexdggh{left:0!important} #mafl5inlineContent{width: 100%!important} #c1dmpinlineContent-gridWrapper{padding: 20px;} #SITE_HEADER{ padding-top: $statusBarHeight !important; } #MENU_AS_CONTAINERoverlay > div{left:0!important;} #c1dmp > div {left:0!important; right:0!important} body.device-mobile-optimized #SITE_CONTAINER {width: 100%!important;}.style-keev1rnv[data-state~="mobileView"] .style-keev1rnvbg {left:0!important; right:0!important;}#QUICK_ACTION_BAR{left:0!important}');
-              styleNode.appendChild(styleText);
-              document.getElementsByTagName('head')[0].appendChild(styleNode);
-              document.getElementById("comp-keewrxoq").remove()
-              document.getElementById("WIX_ADS").remove()
-              document.getElementsByTagName("style").forEach((node) => {node.innerHTML = node.innerHTML.replace("320px", "100%").replace("319px", "100%").replace("260px", "100%");})
-              document.querySelectorAll("[style]").forEach((node) => node.attributes["style"].value = node.attributes["style"].value.replace("320px", "100%").replace("319px", "100%").replace("260px", "100%"));
-              
-              var targetNode = document.getElementById('MENU_AS_CONTAINERoverlay');
-              var callback = function(mutationsList) {
-                  document.getElementById("MENU_AS_CONTAINERoverlay").style.left = 0;
-                  document.getElementById("MENU_AS_CONTAINERoverlay").style.width = "100%";
-              };
-              var observer = new MutationObserver(callback);
-              observer.observe(targetNode, { attributes: true, childList: true });
+            flutterWebViewPlugin.evalJavascript("""
+              setTimeout(function() {
+                var styleNode = document.createElement('style');
+                styleNode.type = "text/css";
+                var styleText = document.createTextNode('#SITE_HEADER{padding-top: $statusBarHeight !important; }');
+                styleNode.appendChild(styleText);
+                document.getElementsByTagName('head')[0].appendChild(styleNode);
+                var script = document.createElement("script");
+                script.src = "https://blinks.app/assets/blinksarea/custom.js?v=${DateTime.now()}";
+                document.head.appendChild(script);
+              }, 500);
             """);
             flutterWebViewPlugin.show();
           }
